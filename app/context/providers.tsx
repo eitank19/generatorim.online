f7f3454464a6dab53c559627f611d7f950f59a42
sohanemon <m.sohanemon@gmail.com>
2024-03-03 07:11:59 +0600

    feat(layout.tsx): updated html class attribute refactor(navbar.tsx): removed unused imports and elements fix(colors.ts): updated color values refactor(providers.tsx): simplified ThemeProvider usage fix(with-redirect.ts): corrected redirect condition

--- app/context/providers.tsx
+++ app/context/providers.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { TailwindIndicator } from '@sohanemon/utils/components';
-import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ThemeProvider defaultTheme="system">
-        {children}
-        <TailwindIndicator />
-      </ThemeProvider>
+      {children}
+      <TailwindIndicator />
     </>
   );
 }
